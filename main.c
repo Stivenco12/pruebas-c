a0a50e176a1c6af55fde5261cf4eae06703e454b
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-05-06 23:58:30 -0500

    feat: :tada: initial proyect

---
+++ main.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("Hello World\n");
+    int a;
+    scanf("%d", &a);
+    int b;
+    scanf("%d", &b);
+    int ab = a + b;
+    printf("%d\n", ab);
+    return ab;
+}
